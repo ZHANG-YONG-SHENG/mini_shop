1581ec55f9dbc1610fbea0af0c4e9ca1428396f5
ZHANG YONG-SHENG <H24663101@mailst.cjcu.edu.tw>
2020-11-04 00:18:48 +0800

    456

--- mini_shop/config.php
+++ mini_shop/config.php
@@ -2,7 +2,7 @@
 define('_SHOP_NAME', 'Tad點心鋪');
 define('_DB_HOST', 'localhost');
 define('_DB_ID', 'root');
-define('_DB_PW', '123456');
+define('_DB_PW', '12345');
 define('_DB_NAME', 'mini_shop');
 define('_UPLOAD_PATH', 'C:/UniServerZ/www/mini_shop/uploads/');
 define('_UPLOAD_URL', 'http://localhost/mini_shop:7777/uploads/');
